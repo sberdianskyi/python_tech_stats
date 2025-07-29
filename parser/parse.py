b96aac1eba2542d45909504dba9c0746323f06c8
Serhii Berdianskyi <zeferot94@gmail.com>
2025-07-29 14:43:46 +0300

    Enhanced error handling and logging in parse.py for vacancy details retrieval and page parsing

--- parser/parse.py
+++ parser/parse.py
@@ -1,6 +1,8 @@
 import csv
 import logging
+import random
 import sys
+import time
 
 from dataclasses import fields, astuple
 from pathlib import Path
@@ -11,12 +13,11 @@ from selenium.common import (
     ElementClickInterceptedException,
     NoSuchElementException,
 )
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.webdriver import WebDriver
 
 from parser.parsing_item import Vacancy
-from technologies import ALL_TECHNOLOGIES
+from parser.technologies import ALL_TECHNOLOGIES
 
 
 PROJECT_ROOT = Path(__file__).parent.parent
@@ -25,9 +26,6 @@ BASE_URL = "https://jobs.dou.ua/"
 CATEGORY = "Python"
 
 _driver: WebDriver | None = None
-
-options = Options()
-options.add_argument("--headless")
 
 
 def get_driver() -> WebDriver:
@@ -52,62 +50,72 @@ logging.basicConfig(
 
 
 def get_vacancy_details(driver: WebDriver, link: str) -> Vacancy:
-    driver.get(link)
+    try:
+        time.sleep(random.uniform(2, 4))
+        driver.get(link)
 
-    title = driver.find_element(By.CLASS_NAME, "g-h2").text
-    company = driver.find_element(By.CSS_SELECTOR, "div.l-n a").text
-    vacancy_description = driver.find_element(By.CLASS_NAME, "l-vacancy").text
-    tech_list = list(
-        set([tech for tech in ALL_TECHNOLOGIES if tech in vacancy_description])
-    )
-    location = driver.find_element(By.CLASS_NAME, "place").text
-    salary = "Not specified"
-    if driver.find_elements(By.CLASS_NAME, "salary"):
-        salary = driver.find_element(By.CLASS_NAME, "salary").text
+        title = driver.find_element(By.CLASS_NAME, "g-h2").text
+        company = driver.find_element(By.CSS_SELECTOR, "div.l-n a").text
+        vacancy_description = driver.find_element(By.CLASS_NAME, "l-vacancy").text
+        tech_list = list(
+            set([tech for tech in ALL_TECHNOLOGIES if tech in vacancy_description])
+        )
+        location = driver.find_element(By.CLASS_NAME, "place").text
+        salary = "Not specified"
+        if driver.find_elements(By.CLASS_NAME, "salary"):
+            salary = driver.find_element(By.CLASS_NAME, "salary").text
 
-    logging.info(f"Got {title} details")
+        logging.info(f"Got {title} details")
 
-    return Vacancy(
-        title=title,
-        company=company,
-        technologies=tech_list,
-        location=location,
-        salary=salary,
-        link=driver.current_url,
-    )
+        return Vacancy(
+            title=title,
+            company=company,
+            technologies=tech_list,
+            location=location,
+            salary=salary,
+            link=driver.current_url,
+        )
+
+    except Exception as e:
+        logging.error(f"Failed to get details for {link}: {str(e)}")
 
 
 def parsing_page():
-    url = f"{BASE_URL}vacancies/?category={CATEGORY}"
-    driver = get_driver()
-    driver.get(url)
+    try:
+        url = f"{BASE_URL}vacancies/?category={CATEGORY}"
+        driver = get_driver()
+        driver.get(url)
 
-    while True:
-        try:
-            logging.info("Clicking 'More' button")
-            more_button = driver.find_element(By.CLASS_NAME, "more-btn")
-            if more_button.is_displayed():
-                more_button.click()
-            else:
+        while True:
+            try:
+                logging.info("Clicking 'More' button")
+                more_button = driver.find_element(By.CLASS_NAME, "more-btn")
+                if more_button.is_displayed():
+                    more_button.click()
+                else:
+                    break
+            except (
+                NoSuchElementException,
+                ElementNotInteractableException,
+                ElementClickInterceptedException,
+            ):
+                logging.info("'More' button is not displayed")
                 break
-        except (
-            NoSuchElementException,
-            ElementNotInteractableException,
-            ElementClickInterceptedException,
-        ):
-            logging.info("'More' button is not displayed")
-            break
 
-    page_vacancies = driver.find_elements(By.CLASS_NAME, "l-vacancy")
-    vacancy_links = [
-        vacancy.find_element(By.CLASS_NAME, "vt").get_attribute("href")
-        for vacancy in page_vacancies
-    ]
+        page_vacancies = driver.find_elements(By.CLASS_NAME, "l-vacancy")
+        vacancy_links = [
+            vacancy.find_element(By.CLASS_NAME, "vt").get_attribute("href")
+            for vacancy in page_vacancies
+        ]
 
-    all_vacancies = [get_vacancy_details(driver, link) for link in vacancy_links]
-    logging.info("Ended parsing page")
+        all_vacancies = [get_vacancy_details(driver, link) for link in vacancy_links]
+        all_vacancies = [v for v in all_vacancies if v is not None]
+        logging.info("Ended parsing page")
 
-    return all_vacancies
+        return all_vacancies
+
+    except Exception as e:
+        logging.error(f"Error during page parsing: {str(e)}")
 
 
 def write_vacancies_to_csv_file(vacancies: list[Vacancy], root: str) -> None:
